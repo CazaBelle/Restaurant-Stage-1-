d329c2e16c9c07bdea3641d8ce36e74892c163a9
Carol Gardiner <carolan.gardiner@gmail.com>
2018-08-02 18:43:36 +0100

    Define service register

---
+++ js/register.js
@@ -0,0 +1,10 @@
+//Register Service Worker//
+registerServiceWorker = () => {
+    if (!navigator.serviceWorker) return;
+  
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      console.log('Service worker registered!')
+    }).catch(() => {
+      console.log('Failed to register service worker.')
+    })
+  }
