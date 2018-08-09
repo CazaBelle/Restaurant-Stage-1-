8c469c335e19b58c4bfc20854455d15ad4010c67
Carol Gardiner <carolan.gardiner@gmail.com>
2018-08-09 19:25:38 +0100

    Corrected service worker

--- js/register.js
+++ js/register.js
@@ -1,10 +1,23 @@
 //Register Service Worker//
-registerServiceWorker = () => {
-    if (!navigator.serviceWorker) return;
+// registerServiceWorker = () => {
+//     if (!navigator.serviceWorker) return;
   
-    navigator.serviceWorker.register('/sw.js').then(() => {
-      console.log('Service worker registered!')
-    }).catch(() => {
-      console.log('Failed to register service worker.')
+//     navigator.serviceWorker.register('/sw.js').then(() => {
+//       console.log('Service worker registered!')
+//     }).catch(() => {
+//       console.log('Failed to register service worker.')
+//     })
+//   }         
+
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker
+    .register('./sw.js', { scope: './' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
     })
-  }         
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+
+}
